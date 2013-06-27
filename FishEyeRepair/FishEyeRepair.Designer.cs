73c93bb6d5b81155f8d4a9a7d595cb6c05214c87
PurpleSun <laoweiyeah@gmail.com>
2013-06-27 12:30:06 +0800

    commit all

---
+++ FishEyeRepair/FishEyeRepair.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace FishEyeRepair
+{
+    partial class FishEyeRepair
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLoadImage = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.pictureBoxBefore = new System.Windows.Forms.PictureBox();
+            this.pictureBoxAfter = new System.Windows.Forms.PictureBox();
+            this.Before = new System.Windows.Forms.Label();
+            this.After = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBefore)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAfter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonLoadImage
+            // 
+            this.buttonLoadImage.Location = new System.Drawing.Point(391, 176);
+            this.buttonLoadImage.Name = "buttonLoadImage";
+            this.buttonLoadImage.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoadImage.TabIndex = 0;
+            this.buttonLoadImage.Text = "Load Image";
+            this.buttonLoadImage.UseVisualStyleBackColor = true;
+            this.buttonLoadImage.Click += new System.EventHandler(this.buttonLoadImage_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(391, 211);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 2;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // pictureBoxBefore
+            // 
+            this.pictureBoxBefore.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBoxBefore.Location = new System.Drawing.Point(12, 29);
+            this.pictureBoxBefore.Name = "pictureBoxBefore";
+            this.pictureBoxBefore.Size = new System.Drawing.Size(373, 474);
+            this.pictureBoxBefore.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxBefore.TabIndex = 4;
+            this.pictureBoxBefore.TabStop = false;
+            // 
+            // pictureBoxAfter
+            // 
+            this.pictureBoxAfter.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBoxAfter.Location = new System.Drawing.Point(472, 29);
+            this.pictureBoxAfter.Name = "pictureBoxAfter";
+            this.pictureBoxAfter.Size = new System.Drawing.Size(373, 474);
+            this.pictureBoxAfter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxAfter.TabIndex = 5;
+            this.pictureBoxAfter.TabStop = false;
+            // 
+            // Before
+            // 
+            this.Before.AutoSize = true;
+            this.Before.Location = new System.Drawing.Point(12, 9);
+            this.Before.Name = "Before";
+            this.Before.Size = new System.Drawing.Size(47, 12);
+            this.Before.TabIndex = 6;
+            this.Before.Text = "Before:";
+            // 
+            // After
+            // 
+            this.After.AutoSize = true;
+            this.After.Location = new System.Drawing.Point(470, 9);
+            this.After.Name = "After";
+            this.After.Size = new System.Drawing.Size(41, 12);
+            this.After.TabIndex = 7;
+            this.After.Text = "After:";
+            // 
+            // FishEyeRepair
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(855, 512);
+            this.Controls.Add(this.After);
+            this.Controls.Add(this.Before);
+            this.Controls.Add(this.pictureBoxAfter);
+            this.Controls.Add(this.pictureBoxBefore);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonLoadImage);
+            this.Name = "FishEyeRepair";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fish Eye Repair";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBefore)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAfter)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLoadImage;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.PictureBox pictureBoxBefore;
+        private System.Windows.Forms.PictureBox pictureBoxAfter;
+        private System.Windows.Forms.Label Before;
+        private System.Windows.Forms.Label After;
+    }
+}
+
---
+++ FishEyeRepair/FishEyeRepair.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace FishEyeRepair
+{
+    public partial class FishEyeRepair : Form
+    {
+        public FishEyeRepair()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLoadImage_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog lvse = new OpenFileDialog())
+            {
+                lvse.Title = "Select a Picture file";
+                lvse.InitialDirectory = "";
+                lvse.Filter = "Type|*.bmp;*.jpg;*.jpeg;*.gif;*.png";
+                lvse.FilterIndex = 1;
+
+                if (lvse.ShowDialog() == DialogResult.OK)
+                {
+                    //TEST CODE BEGIN HERE
+                    //init imageBefore and display it in the pictureBoxBefore
+                    Bitmap imageBefore = new Bitmap(lvse.FileName, true);
+                    pictureBoxBefore.Image = imageBefore;
+                    pictureBoxBefore.Refresh();
+                    //System.Threading.Thread.Sleep(1 * 1000);
+
+                    //begin convert
+                    Bitmap imageAfter = new Bitmap((Image)imageBefore.Clone());
+                    imageAfter = convertToFishEyeType(imageAfter);
+                    pictureBoxAfter.Image = imageAfter;
+                    pictureBoxAfter.Refresh();
+                    //保存变换后的图片
+                    //pictureBoxAfter.Image.Save("after_fisheye.bmp");
+                    //System.Threading.Thread.Sleep(1 * 1000);
+                }
+            }
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonHelp_Click(object sender, EventArgs e)
+        {
+            string htmInfo = @"D:\C-Sharp\FishEyeRepair\index.htm";
+            Process.Start("iexplore.exe", htmInfo);
+        }
+
+        private Bitmap convertToFishEyeType(Bitmap inputBitmap)
+        {
+            int width, height;
+            const float OFFSET_X1 = 20.0f;
+            const float OFFSET_Y1 = 10.0f;
+            int w = inputBitmap.Width;
+            int h = inputBitmap.Height;
+            //R和detaR可调，会得到不同的效果，当R /= 2.8且detaR = 0.0f * R时，可以得到比较接近于圆的效果，但是图片有效区降低
+            //目前来看detaR的值变化不受欢迎
+            float R = ((w >= h) ? (w / 2) : (h / 2));
+            R /= 1.20f;
+            float detaR = 0.0f * R;
+            float aw = 0.0f, ah = 0.0f;
+            float awt2 = 0.0f, aht2 = 0.0f;
+            float awt1 = 0.0f, aht1 = 0.0f;
+
+            Bitmap outputBitmap = new Bitmap((Image)inputBitmap.Clone());
+            //init outputBitmap: set all pixels to black color
+            for (width = 0; width < outputBitmap.Width; width++)
+            {
+                for (height = 0; height < outputBitmap.Height; height++)
+                {
+                    //Color pixelColor = imageAfter.GetPixel(x, y);      //read specified pixels
+                    Color blackColor = Color.FromArgb(0, 0, 0);
+                    outputBitmap.SetPixel(width, height, blackColor);    //write specified pixels
+                }
+            }
+            //convert
+#if false 
+            for (height = 0; height < outputBitmap.Height; height++)//bh
+            {
+                for (width = 0; width < outputBitmap.Width; width++)//bw
+                {
+                    awt1 = width - w / 2;
+                    aht1 = height - h / 2;
+
+                    float temp = (float)(Math.Sqrt((float)Math.Pow(awt1, 2) + (float)Math.Pow(aht1, 2) + (float)Math.Pow((R + detaR), 2)));
+                    awt2 = (awt1 * R) / temp;
+                    aht2 = (aht1 * R) / temp;
+                    aw = awt2 + w / 2;
+                    ah = aht2 + h / 2;
+                    Color newColor = inputBitmap.GetPixel(width, height);
+                    outputBitmap.SetPixel((int)aw, (int)ah, newColor);
+                }
+            }
+#else
+            for (height = 0; height < outputBitmap.Height; height++)//bh
+            {
+                for (width = 0; width < outputBitmap.Width; width++)//bw
+                {
+                    awt1 = width - w / 2 - OFFSET_X1;
+                    aht1 = height - h / 2 - OFFSET_Y1;
+
+                    float temp = (float)(Math.Sqrt((float)Math.Pow(awt1, 2) + (float)Math.Pow(aht1, 2) + (float)Math.Pow((R + detaR), 2)));
+                    awt2 = (awt1 * R) / temp;
+                    aht2 = (aht1 * R) / temp;
+
+                    /*增大视场大小*/
+                    //awt2 /= 0.85f;
+                    //aht2 /= 0.85f;
+
+                    aw = awt2 + w / 2 + OFFSET_X1;
+                    ah = aht2 + h / 2 + OFFSET_Y1;
+
+                    Color newColor = inputBitmap.GetPixel((int)aw, (int)ah);
+                    outputBitmap.SetPixel((int)width, (int)height, newColor);
+                }
+            }
+#endif
+            return outputBitmap;
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
